f2cbb8cbff72e1463ca17e9af7aa2067f2a68904
Erland Ruiz <erlandruiz@hotmail.com>
2023-07-07 16:47:49 -0500

    sacando NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
-import { NavBar } from './components/NavBar/NavBar'
-import { Item } from './components/Item/Item'
+// import { NavBar } from './components/NavBar/NavBar'
+// import { Item } from './components/Item/Item'
 
 // import { NavBar, Item } from './components'
 
